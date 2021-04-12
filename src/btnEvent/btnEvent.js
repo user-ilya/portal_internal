1cd855fba6a4ddcf11e11e1a2b6959ced41083bc
Ilya <ILYAkuzmin31072001@yandex.ru>
2021-04-12 19:13:49 +0300

    add changes

--- src/btnEvent/btnEvent.js
+++ src/btnEvent/btnEvent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/btn.css';
 
 const EventBtn = () => {
@@ -18,15 +18,27 @@ const EventBtn = () => {
     const opacity = document.querySelector('#semi_opacity')
     const modal = document.querySelector('#modalWindow');
 
-    btn.addEventListener('click', () => {
-        modal.classList.add('modal_active')
-        opacity.classList.add('semi-opacity_active')
-    })
+/*     btn.addEventListener('click', () => {
+        modal.classList.toggle('modal_active')
+        opacity.classList.toggle('semi-opacity_active')
+    }) */
+
+    const {statics, active} = useState(opacity, modal)
     
+    const onClickBtnAdd = (statics) => {
+        statics = active(() => {
+                modal.classList.toggle('modal_active')
+                opacity.classList.toggle('semi-opacity_active')
+            }
+        )
+    }
 
     return(
         <div className='btnMain'>
-            <button type = 'button' className='btnEvent'>Добавить событие</button>
+            <button 
+                type = 'button' 
+                onClick = {this.onClickBtnAdd()}
+                className='btnEvent'>Добавить событие</button>
         </div>
     )
 }
